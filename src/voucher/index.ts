7d54681d6be148a747991aad622322a97f503d7b
Matthew Abraham <wasteofintel@gmail.com>
2020-05-23 22:32:03 -0400

    add Voucher:Check functionality

--- src/voucher/index.ts
+++ src/voucher/index.ts
@@ -1,4 +1,12 @@
 import WiPayAuth from "../auth";
+import axios from 'axios';
+
+interface WiPayVoucherResponse {
+    status: String,
+    msg: String,
+    trxn_id?: String,
+    value?: Number,
+}
 
 class WiPayVoucher {
     private _authorisation: WiPayAuth;
@@ -7,8 +15,24 @@ class WiPayVoucher {
         this._authorisation = authorisation;
     }
 
-    public check = () : Boolean => {
-        return false;
+    public check = async (voucher: String) : Promise<WiPayVoucherResponse> => {
+        if (voucher.length != 12) throw new Error("Invalid Voucher Length");
+        const response = await axios.post(
+            this._authorisation.Endpoint +
+            'voucher_check',
+            {
+                voucher: voucher
+            }
+        );
+
+        const result: WiPayVoucherResponse = {
+            status: response.data.status,
+            msg: response.data.msg,
+            value: response.data.value
+        }
+
+        return result;
+    }
     }
 
     public pay = () : any => {
