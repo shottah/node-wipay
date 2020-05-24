6d3a39cdbacd1e830f248c8dcd62f033d390d11b
Matthew Abraham <wasteofintel@gmail.com>
2020-05-23 22:31:09 -0400

    example of voucher-check and voucher-pay

--- example/index.js
+++ example/index.js
@@ -1,6 +1,17 @@
-const WiPayAuth = require('../lib');
+const WiPayAuth = require('../lib').WiPayAuth;
+const WiPayVoucher = require('../lib').WiPayVoucher
 
 const api = WiPayAuth.getInstance({
-    AccountID: 28937482375,
+    AccountNumber: 4630,
     API_KEY: "u9ufe8afj89ea",
+    Sandbox: true,
 })
+
+const vou = new WiPayVoucher(api);
+vou.check('uyr8vmap230t')
+.catch(res => console.log(res))
+.then(err => console.log(err));
+
+vou.pay('uyr8vmap230t', 0.01, "Voucher payment for testing.")
+.then(res => console.log(res))
+.catch(err => console.log(err));
