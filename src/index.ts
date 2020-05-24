ddbb0b29d4baa1e3d4eb68e5b8ef1a38f2da2e57
Matthew Abraham <wasteofintel@gmail.com>
2020-05-24 14:02:22 -0400

    Export GatewayAPI

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 import WiPayAuth from "./auth";
 import WiPayVoucher from './voucher';
-export {WiPayAuth, WiPayVoucher};
+import WiPayGateway from './creditcard';
+export {WiPayAuth, WiPayVoucher, WiPayGateway};
