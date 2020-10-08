4cb7d5cb5ffcccf94b167fa8b8308852370e9819
Matthew Abraham <matthew@davyntt.com>
2020-10-07 22:07:12 -0400

    Update livemode selection.

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -27,9 +27,9 @@ class WiPayAuth {
    */
   private constructor(config: WiPayAuthConfig) {
     WiPayAuth._config = config;
-    WiPayAuth._endpoint = config.LiveMode ?  API.Live : API.Sandbox
-    WiPayAuth._gateway = config.LiveMode ?  Gateway.Live: Gateway.Sandbox;
-    WiPayAuth._LiveMode = config.LiveMode ? true : false;
+    WiPayAuth._endpoint = !config.LiveMode ?  API.Live : API.Sandbox
+    WiPayAuth._gateway = !config.LiveMode ?  Gateway.Live: Gateway.Sandbox;
+    WiPayAuth._LiveMode = !config.LiveMode ? true : false;
   }
 
   /**
